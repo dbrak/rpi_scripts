98564c9629a530f96650423f290fda3a3814ab62
jbrak <joe@brakoniecki.com>
2015-02-22 21:03:15 +0000

    Adding spiral.py script to repository.  Updating Author

---
+++ Graphics/spiral.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import turtle
+import math
+
+num = raw_input("How many squares do you want?")
+length = 100
+angle = 360 / int(num)
+
+
+wn = turtle.Screen()
+wn.bgcolor("green")
+wn.title("Spiral")
+
+t = turtle.Pen()
+t.pensize(1)
+t.color("blue")
+t.speed(60)
+
+
+def square():
+    t.forward(length)
+    t.left(90)
+    t.forward(length)
+    t.left(90)
+    t.forward(length)
+    t.left(90)
+    t.forward(length)
+    t.left(90)
+
+for x in range (0,int(num)+1):
+    square()
+    t.left(angle)
+
+turtle.done()
+    
