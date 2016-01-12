e79ac17f5c7a0b5fd30d61b00b77e832f48b577c
Jbrak <joe@brakoniecki.com>
2016-01-12 18:18:08 +0000

    New folder

---
+++ Python Ex/Coding_Glossary.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+window = TK ()
+window.title("Coding Glossary")
+
+
+my_glossary = {
+	'algorithm': 'Step by step instructions to perform a task that a computer could understand.'
+	'bug': 'A piece of code that is causing a program to fail to run properly.'
+	'binary number': ' A number represented in base 2(2 numbers).'
+	}
+	
+	
+window.mainloop
