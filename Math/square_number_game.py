51bc5abd8a16297132726a75caa3154456d8cb01
Jbrak <joe@brakoniecki.com>
2016-04-23 12:07:38 +0100

    new python gamne

---
+++ Math/square_number_game.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from random import randint
+
+s = 0
+comp_s = 0
+print "----------"
+ 
+
+for i in range(1,100):
+
+
+	x = randint(1,8)
+	print "Your integer is %d" % x 
+
+	count = 0
+	while(count<3):
+		sq = input("What is your number squared:  ") 
+
+		if sq == x*x:
+			print "Correct"
+			break
+		else:
+			print "Wrong, Try Again"
+			count += 1
+	else: 
+		print "Sorry, too many tries.  You lose!"
+
+
+	count = 0
+	while(count<3):
+		sc = input("Your last score was %d. What is your new one:  " % s) 
+
+		if sc == s + sq:
+			print "Correct"
+			s += sq
+			break
+		else:
+			print "Wrong, Try Again"
+			count += 1
+	else: 
+		print "Sorry, too many tries.  You lose!"
+
+	if s >= 100:
+		print "You won!!!"
+		break
+
+	x = randint(1,8) 	
+	print "----------"	
+	print "My Turn"
+	print "My number is %d." % x 
+	print "It's square is %d" % (x*x)
+	comp_s += x*x
+	print "My score is %d" % comp_s
+	print "----------"
+	
+	if comp_s >= 100:
+		print "I won!!!"
+		break 
+	
+	
